30fbd8d9c725a56efe96ed5d26b85a37524013e3
Sagar Sarkar <sarkar.sagar@gmail.com>
2019-11-23 18:49:59 +0530

    contact form and email sending

--- contacts/admin.py
+++ contacts/admin.py
@@ -6,9 +6,10 @@ def get_realtor_name(obj):
    listing =  Listing.objects.get(id=obj.listing_id)
    return listing.realtor.name
 
-get_realtor_name.short_description = "Realtor"    
+get_realtor_name.short_description = "Realtor"   
+
 class ContactAdmin(admin.ModelAdmin):
-     list_display = ('id','name','email','phone',get_realtor_name)
+     list_display = ('id','name','listing','email','phone',get_realtor_name)
      list_display_links = ('id','name')
      list_per_page = 10
      list_filter = ('name','email')
--- contacts/views.py
+++ contacts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from listings.models import Listing
 from .models import Contact
 from django.contrib import messages
+from django.core.mail import send_mail
 
 def index(request):
     if request.method == "POST":
@@ -15,10 +16,23 @@ def index(request):
             message = request.POST['message']
             if request.user.id is not None:
                user_id = request.user.id
+               is_contacted =  Contact.objects.filter(user_id=user_id,listing_id=listing_id)
+               if is_contacted:
+                   messages.error(request,"You already contacted for this property")
+                   return redirect("/listings/"+listing_id)
+
             else:
                user_id = 0    
             contact = Contact(listing=listing_title,listing_id=listing_id,name=name,email=email,phone=phone,message=message,user_id=user_id)
             contact.save()
+            #send email
+            send_mail(
+                'Property Enquiry',
+                'Thanks for showing interest in '+listing_title,
+                'smtpsagar19@gmail.com',
+                [email],
+                fail_silently=False,
+            )
             messages.success(request,"Thanks for showing interest. Our realtor will get back to you soon")
         else:
             messages.error(request,"There is some issue. Please check and try later")
